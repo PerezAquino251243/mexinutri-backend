e00422feae22064651f494fcf47417f043a679b2
PerezAquino251243 <251243@ids.upchiapas.edu.mx>
2026-07-14 20:36:26 -0600

    Despliegue

--- mexinutri-api/src/config/env.ts
+++ mexinutri-api/src/config/env.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 import path from 'path';
 
-dotenv.config({ path: path.resolve(__dirname, '../../.env') });
+dotenv.config();
 
 export const env = {
   port: Number(process.env.PORT ?? 3000),
